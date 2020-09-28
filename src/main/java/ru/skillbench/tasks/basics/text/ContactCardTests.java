bef51e8d0345b4cf6024df4865eea3a5d7c62256
Disteonne <diana.razmikovna@yandex.ru>
2020-09-28 12:09:56 +0300

    ContactCard vers 1 11/12

--- src/main/java/ru/skillbench/tasks/basics/text/ContactCardTests.java
+++ src/main/java/ru/skillbench/tasks/basics/text/ContactCardTests.java
@@ -19,12 +19,25 @@ import org.junit.rules.Timeout;
 public class ContactCardTests {
     private static final String GOOD[] = {
             "FN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nGENDER:M\r\nTEL;TYPE=HOME:4951234567",
-            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\nTEL;TYPE=WORK:1234567890",
+            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940",
+            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\nGENDER:M\r\nTEL;TYPE=WORK:1234567890",
+            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\nGENDER:F\r\nTEL;TYPE=WORK:1234567890",
+            "FN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nGENDER:M\r\nTEL;TYPE=WORK:1234567890"
+
     };
+
     private static final String BAD[] = {
             "FN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nGENDER:M\r\nBDAY:06-06-1944\r\nTEL;TYPE=HOME:+1 234-567", //wrong phone format
             "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\nTEL;TYPE=WORK:12345678901",
-            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\nGENDER:M\r\nTEL;TYPE=WORK:12345678901"//digits > 10
+            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\nGENDER:k\r\nTEL;TYPE=WORK:1234567890",
+            "ORG:Hollywood\r\nBDAY:10-04-1940\r\nGENDER:k\r\nTEL;TYPE=WORK:1234567890",
+            "FN:Chuck Norris\r\nBDAY:10-04-1940\r\nGENDER:k\r\nTEL;TYPE=WORK:1234567890",
+            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-1940\r\nGENDER:m\r\nTEL;TYPE=WORK:1234567890",
+            "FN:Chuck Norris\r\nORG:Hollywood\r\nBDAY:10-04-1940\r\n"
+
+
+
+            //digits > 10
     };
 
     private ContactCard impl = new ContactCardImpl();
@@ -34,6 +47,12 @@ public class ContactCardTests {
 
     private ContactCard getCard(String text){
         return impl.getInstance("BEGIN:VCARD\r\n"+text+"\r\nEND:VCARD");
+    }
+    private ContactCard getCardExNoBegin(String textTwo){
+        return impl.getInstance(textTwo+"\r\nEND:VCARD");
+    }
+    private ContactCard getCardExNoEND(String textThree){
+        return impl.getInstance("BEGIN:VCARD\r\n"+textThree);
     }
 
     @Test
@@ -82,13 +101,16 @@ public class ContactCardTests {
     }
 
 
+
     @Test
     public void getPhone1(){
-        String text = GOOD[1];
+        String text = GOOD[4];
         ContactCard card = getCard(text);
+        String get=card.getPhone("WORK");
         assertEquals("getPhone(\"WORK\") failed with this text:\n"+text+"\n",
                 "(123) 456-7890", card.getPhone("WORK"));
     }
+
 
     @Test(expected = InputMismatchException.class)
     public void phoneExceptionNotNumber(){
@@ -99,11 +121,58 @@ public class ContactCardTests {
     public void phoneExceptionNot10Digits(){
         getCard(BAD[1]);
     }
+
     //TEST!!!
+    @Test
+    public void wrongExGenderWOMAN(){
+        String str=GOOD[3];
+        ContactCard contactCard=getCard(str);
+        assertEquals("res",true, contactCard.isWoman());
+    }
+
+    @Test
+    public void wrongExGenderMAN(){
+        String str=GOOD[2];
+        ContactCard contactCard=getCard(str);
+        assertEquals(false,contactCard.isWoman());
+    }
+
+    @Test
+    public void noGender(){
+        String str=GOOD[1];
+        ContactCard contactCard=getCard(str);
+        assertEquals(false,contactCard.isWoman());
+    }
     @Test(expected = InputMismatchException.class)
-    public void wrongExGender(){
-        //String str=BAD[2];
-        ContactCard contactCard=getCard(BAD[2]);
-        assertEquals("res",false, impl.isWoman());
+    public void genderEx(){
+        getCard(BAD[2]);
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void noBEGIN(){
+        getCardExNoBegin(GOOD[1]);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void noEND(){
+        getCardExNoEND(GOOD[1]);
+    }
+
+
+    @Test(expected = NoSuchElementException.class)
+    public void noFN(){
+        getCard(BAD[3]);
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void noORG(){
+        getCard(BAD[4]);
+    }
+    @Test(expected = InputMismatchException.class)
+    public void wrongBirthdayFormat(){
+        getCard(BAD[5]);
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void noPhone(){
+        getCard(BAD[6]);
     }
 }
