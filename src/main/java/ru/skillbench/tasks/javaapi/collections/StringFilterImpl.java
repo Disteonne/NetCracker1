ecee3bac4e90420023bdd805c38a938e91a37486
Disteonne <diana.razmikovna@yandex.ru>
2020-10-07 10:12:17 +0300

    task ONE vers2 READY(1)

--- src/main/java/ru/skillbench/tasks/javaapi/collections/StringFilterImpl.java
+++ src/main/java/ru/skillbench/tasks/javaapi/collections/StringFilterImpl.java
@@ -1,12 +1,10 @@
 package ru.skillbench.tasks.javaapi.collections;
 
-import java.lang.reflect.Array;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StringFilterImpl implements StringFilter {
     private Set<String> set = new LinkedHashSet<>();
+    private ArrayList<String> arrayList = new ArrayList<>();
     private Filter filter = new Filter() {
         @Override
         public boolean validator(String s) {
@@ -16,6 +14,7 @@ public class StringFilterImpl implements StringFilter {
                 return false;
         }
     };
+    private boolean isEmpty = true;
 
     public StringFilterImpl() {
         super();
@@ -23,62 +22,86 @@ public class StringFilterImpl implements StringFilter {
 
     @Override
     public void add(String s) {
-        set.add(s.toLowerCase());
+        boolean isTrue=false;
+        //set.add(s.toLowerCase());
+        if (isEmpty == true) {
+            arrayList.add(s);
+        } else {
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i) == s) {
+                    isTrue=true;
+                }
+            }
+            if(isTrue!=true){
+                arrayList.add(s);
+            }
+        }
+        isEmpty = false;
     }
 
     @Override
     public boolean remove(String s) {
-        return set.remove(s.toLowerCase());
-    }
-
-    @Override
-    public void removeAll() {
-        for (String s : set
-        ) {
-            remove(s);
+        //return set.remove(s.toLowerCase());
+        if (s == null) {
+            return arrayList.remove(null);
+        } else {
+            return arrayList.remove(s.toLowerCase());
         }
     }
 
     @Override
+    public void removeAll() {
+        arrayList.removeAll(arrayList);
+        /*
+        for (String s : set
+        ) {
+            remove(s);
+        }
+         */
+    }
+
+    @Override
     public Collection<String> getCollection() {
-        return set;
+        return arrayList;
     }
 
     @Override
     public Iterator<String> getStringsContaining(String chars) {
         ArrayList<String> list = new ArrayList<>();
-        list.addAll(set);
-        ArrayList<String> resultList=new ArrayList<>();
+        list.addAll(arrayList);
+        ArrayList<String> resultList = new ArrayList<>();
 
         if (filter.validator(chars) == true) {
-            return set.iterator();
+            return arrayList.iterator();
         } else {
             return new Iterator<String>() {
-                int counter=0;
-                int countHasNext=0;
+                int counter = 0;
+                int countHasNext = 0;
+
                 @Override
                 public boolean hasNext() {
-                    boolean isNext=false;
-                    if(countHasNext==0) {
+                    boolean isNext = false;
+                    if (countHasNext == 0) {
                         for (int i = 0; i < list.size(); i++) {
-                            if (list.get(i).contains(chars.toLowerCase())) {
-                                isNext = true;
-                                resultList.add(list.get(i));
-                                countHasNext=resultList.size();
+                            if(list.get(i)!=null) {
+                                if (list.get(i).contains(chars.toLowerCase())) {
+                                    isNext = true;
+                                    resultList.add(list.get(i));
+                                    countHasNext = resultList.size();
+                                }
                             }
                         }
-                    }else if(counter<countHasNext){
-                        isNext=true;
-                    }
-                    else
-                        isNext=false;
+                    } else if (counter < countHasNext) {
+                        isNext = true;
+                    } else
+                        isNext = false;
                     return isNext;
                 }
 
                 @Override
                 public String next() {
-                        return resultList.get(counter++);
-                    }
+                    return resultList.get(counter++);
+                }
             };
         }
     }
@@ -87,31 +110,33 @@ public class StringFilterImpl implements StringFilter {
     @Override
     public Iterator<String> getStringsStartingWith(String begin) {
         ArrayList<String> list = new ArrayList<>();
-        list.addAll(set);
-        ArrayList<String> resultList=new ArrayList<>();
+        list.addAll(arrayList);
+        ArrayList<String> resultList = new ArrayList<>();
 
         if (filter.validator(begin) == true) {
-            return set.iterator();
+            return arrayList.iterator();
         } else {
             return new Iterator<String>() {
-                int counter=0;
-                int countHasNext=0;
+                int counter = 0;
+                int countHasNext = 0;
+
                 @Override
                 public boolean hasNext() {
-                    boolean isNext=false;
-                    if(countHasNext==0) {
+                    boolean isNext = false;
+                    if (countHasNext == 0) {
                         for (int i = 0; i < list.size(); i++) {
-                            if (list.get(i).startsWith(begin.toLowerCase())) {
-                                isNext = true;
-                                resultList.add(list.get(i));
-                                countHasNext=resultList.size();
+                            if(list.get(i)!=null) {
+                                if (list.get(i).startsWith(begin.toLowerCase())) {
+                                    isNext = true;
+                                    resultList.add(list.get(i));
+                                    countHasNext = resultList.size();
+                                }
                             }
                         }
-                    }else if(counter<countHasNext){
-                        isNext=true;
-                    }
-                    else
-                        isNext=false;
+                    } else if (counter < countHasNext) {
+                        isNext = true;
+                    } else
+                        isNext = false;
                     return isNext;
                 }
 
@@ -126,33 +151,35 @@ public class StringFilterImpl implements StringFilter {
     @Override
     public Iterator<String> getStringsByNumberFormat(String format) {
         ArrayList<String> list = new ArrayList<>();
-        list.addAll(set);
-        ArrayList<String> resultList=new ArrayList<>();
+        list.addAll(arrayList);
+        ArrayList<String> resultList = new ArrayList<>();
 
         if (filter.validator(format) == true) {
-            return set.iterator();
+            return arrayList.iterator();
         } else {
             return new Iterator<String>() {
-                int counter=0;
-                int countHasNext=0;
+                int counter = 0;
+                int countHasNext = 0;
+
                 @Override
                 public boolean hasNext() {
-                    boolean isNext=false;
-                    if(countHasNext==0) {
+                    boolean isNext = false;
+                    if (countHasNext == 0) {
                         for (int i = 0; i < list.size(); i++) {
-                            String str = list.get(i);
-                            str = str.replaceAll("[0-9]", "#");
-                            if (str.equals(format.toLowerCase())) {
-                                isNext = true;
-                                resultList.add(list.get(i));
-                                countHasNext=resultList.size();
+                            if(list.get(i)!=null) {
+                                String str = list.get(i);
+                                str = str.replaceAll("[0-9]", "#");
+                                if (str.equals(format.toLowerCase())) {
+                                    isNext = true;
+                                    resultList.add(list.get(i));
+                                    countHasNext = resultList.size();
+                                }
                             }
                         }
-                    }else if(counter<countHasNext){
-                        isNext=true;
-                    }
-                    else
-                        isNext=false;
+                    } else if (counter < countHasNext) {
+                        isNext = true;
+                    } else
+                        isNext = false;
                     return isNext;
                 }
 
@@ -167,34 +194,36 @@ public class StringFilterImpl implements StringFilter {
     @Override
     public Iterator<String> getStringsByPattern(String pattern) {
         ArrayList<String> list = new ArrayList<>();
-        list.addAll(set);
-        ArrayList<String> resultList=new ArrayList<>();
+        list.addAll(arrayList);
+        ArrayList<String> resultList = new ArrayList<>();
 
         if (filter.validator(pattern) == true) {
-            return set.iterator();
+            return arrayList.iterator();
         } else {
             return new Iterator<String>() {
-                int counter=0;
-                int countHasNext=0;
+                int counter = 0;
+                int countHasNext = 0;
+
                 @Override
                 public boolean hasNext() {
-                    boolean isNext=false;
-                    String patternString=pattern.replaceAll("[*]","[a-z]{0,}");
-                    if(countHasNext==0) {
+                    boolean isNext = false;
+                    String patternString = pattern.replaceAll("[*]", "[a-z]{0,}");
+                    if (countHasNext == 0) {
 
                         for (int i = 0; i < list.size(); i++) {
-                            String str = list.get(i);
-                            if (str.matches(patternString)) {
-                                isNext = true;
-                                resultList.add(list.get(i));
-                                countHasNext=resultList.size();
+                            if (list.get(i) != null) {
+                                String str = list.get(i);
+                                if (str.matches(patternString)) {
+                                    isNext = true;
+                                    resultList.add(list.get(i));
+                                    countHasNext = resultList.size();
+                                }
                             }
                         }
-                    }else if(counter<countHasNext){
-                        isNext=true;
-                    }
-                    else
-                        isNext=false;
+                    } else if (counter < countHasNext) {
+                        isNext = true;
+                    } else
+                        isNext = false;
                     return isNext;
                 }
 
@@ -226,6 +255,7 @@ public class StringFilterImpl implements StringFilter {
         stringFilter.add("what are u doing, mom?");
         stringFilter.add("mam 1 555");
         stringFilter.add("1 999 cat");
+        stringFilter.add(null);
         stringFilter.add("9");
         stringFilter.add("-9.32");
         stringFilter.add("-7.432");
@@ -233,8 +263,8 @@ public class StringFilterImpl implements StringFilter {
         stringFilter.add("almati");
         stringFilter.add("a2m");
         stringFilter.add("abudabimani");
-        /*
-        Iterator iterator = stringFilter.getStringsContaining("e");
+
+        Iterator iterator = stringFilter.getStringsContaining("mom");
 
         while (iterator.hasNext()) {
            // String str= (String) iterator.next();
@@ -242,7 +272,6 @@ public class StringFilterImpl implements StringFilter {
 
         }
 
-         */
 
 
         /*
@@ -254,13 +283,15 @@ public class StringFilterImpl implements StringFilter {
 
          */
 
+
         /*
-        Iterator iterator2 = stringFilter.getStringsByNumberFormat("#");
+        Iterator iterator2 = stringFilter.getStringsByNumberFormat("# ###");
         //ArrayList<String> s=new ArrayList<>();
         while (iterator2.hasNext()){
            System.out.println(iterator2.next());
         }
          */
+
 
 
         /*
@@ -270,10 +301,13 @@ public class StringFilterImpl implements StringFilter {
         }
          */
 
-        Iterator iterator4=stringFilter.getStringsByPattern("*m");
+        /*
+        Iterator iterator4=stringFilter.getStringsByPattern("*m*");
         while (iterator4.hasNext()){
             //String s=(String) iterator4.next();
             System.out.println(iterator4.next());
         }
+         */
     }
 }
+
