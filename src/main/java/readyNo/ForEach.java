8e6c71d3ca27755199375b39c72cb63d74372534
Disteonne <diana.razmikovna@yandex.ru>
2020-11-23 23:38:16 +0300

    interview 1

--- src/main/java/readyNo/ForEach.java
+++ src/main/java/readyNo/ForEach.java
@@ -1,6 +1,13 @@
 package readyNo;
 
 public class ForEach {
+    //Перегрузка конструктора
+    public ForEach(String st) {
+    }
+    public ForEach(int i){
+
+    }
+
     public static void main(String[] args) {
         int[] array= {1,2,3,4,5};
         //Только чтение
@@ -13,5 +20,11 @@ public class ForEach {
             System.out.println(array[i]);
         }
 
+    }
+}
+class test{
+    public static void main(String[] args) {
+       ForEach forEach=new ForEach("kek");
+       ForEach forEach1=new ForEach(5);
     }
 }
