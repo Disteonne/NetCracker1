8dbce57376d00300c9013c9a6d173d88e26ed8bc
Disteonne <diana.razmikovna@yandex.ru>
2020-11-25 23:28:57 +0300

    interview 4

---
+++ src/main/java/readyNo/Io.java
@@ -0,0 +1,62 @@
+package readyNo;
+
+import jdk.internal.jline.internal.InputStreamReader;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Io {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNext()){
+            String s=scanner.nextLine().toString();
+        }
+
+        //АБСТРАКТНЫЕ
+        InputStream inputStream=new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 0;
+            }
+        };
+        OutputStream outputStream=new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+
+            }
+        };
+        Reader reader=new Reader() {
+            @Override
+            public int read(char[] cbuf, int off, int len) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public void close() throws IOException {
+
+            }
+        };
+        Writer writer=new Writer() {
+            @Override
+            public void write(char[] cbuf, int off, int len) throws IOException {
+
+            }
+
+            @Override
+            public void flush() throws IOException {
+
+            }
+
+            @Override
+            public void close() throws IOException {
+
+            }
+        };
+        //НЕ АБСТРАКТНЫЕ
+        //InputStreamReader inputStreamReader=new InputStreamReader();
+        //OutputStreamWriter outputStreamWriter=new OutputStreamWriter();
+        //FileWriter fileWriter=new FileWriter();
+        //ByteArrayInputStream byteArrayInputStream=new ByteArrayInputStream();
+        //DataInputStream dataInputStream=new DataInputStream();
+    }
+}
---
+++ src/main/java/readyNo/SetIterator.java
@@ -0,0 +1,13 @@
+package readyNo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class SetIterator {
+    public static void main(String[] args) {
+        Set set=new HashSet<String>();
+        Map<Integer,String> map=new HashMap<>();
+    }
+}
